9c0f131f06b491cf6a18452adbe4c4a6262fae83
ElysiaKafka <38758499+lucifermbbgh@users.noreply.github.com>
2024-02-13 15:12:36 +0800

    更改项目结构

--- webpack.config.prod.js
+++ webpack.config.prod.js
@@ -12,8 +12,8 @@ module.exports = merge(common, {
     new CopyPlugin({
       patterns: [
         { from: 'img', to: 'img' },
-        { from: 'css', to: 'css' },
-        { from: 'js/vendor', to: 'js/vendor' },
+        { from: 'src/css', to: 'src/css' },
+        { from: 'src/js', to: 'src/js' },
         { from: 'icon.svg', to: 'icon.svg' },
         { from: 'favicon.ico', to: 'favicon.ico' },
         { from: 'robots.txt', to: 'robots.txt' },
